b4ddc2f10d110841b9b0edbd92f85a2fd2db7b28
Wyna <98083311+Wyna2@users.noreply.github.com>
2022-04-14 00:20:00 +0900

    commit 220414

--- AlgorithmStudy/src/DoIt_02_1/Training2_3_CloneArray.java
+++ AlgorithmStudy/src/DoIt_02_1/Training2_3_CloneArray.java
@@ -4,9 +4,9 @@ public class Training2_3_CloneArray {
 
 	public static void main(String[] args) {
 		int[] a = {1,2,3,4,5};
-		int[] b = a.clone(); //b는 a가 참조하는 배열 본체의 복제를 참조
+		int[] b = a.clone(); //b는 a가 참조하는 배열 본체의 복제를 참조하므로 int[] b = {1,2,3,4,5};
 		
-		b[3] = 0; // 0 대입
+		b[3] = 0; // b[3]=4 에 0을 대입함
 		
 		System.out.print("a ="); // 1 2 3 4 5
 		for(int i=0; i<a.length; i++)
